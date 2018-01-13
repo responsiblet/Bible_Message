93cb57061020b9118a228b8ad0c0f5bf65ec356b
andy turner <responsiblet@gmail.com>
2018-01-12 23:12:22 -0600

    Update app_backup.js

--- app_backup.js
+++ app_backup.js
@@ -4,8 +4,8 @@ var authToken = '';
 var client = new twilio.RestClient(accountSid, authToken); 
 client.messages.create({ 
     body: 'Greetings earthling, this is the TwilioSmsBot ;)', 
-    to: '+12174918770',  // Number that receives the SMS 
-    from: '+12178826175' // Purchased Twilio number that send the SMS 
+    to: '+123456789',  // Number that receives the SMS 
+    from: '+123456789' // Purchased Twilio number that send the SMS 
 }, 
 function(err, message) { 
     console.log(message.sid); 
